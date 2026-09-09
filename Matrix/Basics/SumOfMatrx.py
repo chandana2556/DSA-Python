8c0ed1e1344e00d25c34968ec303f4fbd69f2eda
chandana2556 <madugulachandana2625@gmail.com>
2026-09-09 22:18:49 +0530

    Added Sum of Matrix

---
+++ Matrix/Basics/SumOfMatrx.py
@@ -0,0 +1,13 @@
+
+#Sum of the matrix
+
+m=[[1,2,3,4],[5,6,7,8],[9,0,1,2]]
+rows=len(m)
+cols=len(m[0])
+total=0
+for i in range(rows):
+    for j in range(cols):
+        total=total+m[i][j]
+        print(m[i][j],end=" ")
+    print()
+print("Sum = ",total)
